57fe5efe2ce9226c413d046696be7b6483186c48
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-06-19 20:40:25 +0530

    Time: 66 ms (78.59%), Space: 26.3 MB (85.68%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -7,8 +7,9 @@ public:
         for (int i = 0; i < searchWord.length(); i++) {
             vector<string> res;
             char c = searchWord[i];
-            while (left <= right && (products[left].length() == i || products[left][i] < c)) left++;
-            while (left <= right && (products[right].length() == i || products[right][i] > c)) right--;
+            while (left <= right && (products[left].length() == i ||                                  products[left][i] < c)) left++;
+            while (left <= right && (products[right].length() == i ||                                  products[right][i] > c)) right--;
+           
             for (int j = 0; j < 3 && left + j <= right; j++)
                 res.push_back(products[left+j]);
             ans.push_back(res);
