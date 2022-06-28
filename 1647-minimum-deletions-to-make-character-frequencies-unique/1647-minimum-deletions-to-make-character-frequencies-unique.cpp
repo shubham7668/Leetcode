f5eb5fc7b8dd2e1b74a37c555c8850ed19dbec17
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-06-28 08:04:02 +0530

    Time: 59 ms (85.39%), Space: 17.3 MB (58.78%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        vector<int> v(26);
+        
+        for(int i=0;i<s.length();i++){
+            v[s[i]-'a']++;
+        }
+        
+        sort(v.begin(),v.end(),greater<int>());  //Sort in decreasing order
+        // for(int i=0;i<v.size();i++){
+        //     cout<<v[i]<<" ";
+        // }
+        int freq=v[0];       //Maximum allowed frequency
+        int ans=0;
+        
+        for(int i=0;i<v.size();i++){
+            if(v[i]>freq)
+            {
+                if(freq>0) ans+=(v[i]-freq);
+                else
+                    ans+=v[i];
+            }
+            freq=min(freq-1,v[i]-1);
+        }
+        return ans;
+    }
+};
