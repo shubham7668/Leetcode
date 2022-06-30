e3adef0df9f48826293ac644c0340f7f7c5fde85
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-07-01 02:16:40 +0530

    Time: 22 ms (31.06%), Space: 20.9 MB (6.98%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -11,25 +11,28 @@
  */
 class Solution {
 private:
-    int height(TreeNode* root){
-        if(root == nullptr) return 0;
+    pair<int,int> diameter1(TreeNode* root){
         
-        int left=height(root->left);
-        int right=height(root->right);
+		if(root == nullptr ){             //first block=diameter, Second block=height
+            pair<int,int> p = make_pair(0,0);
+            return p;
+        } 
         
-        int ans= max(left,right)+1;
-        return ans;
+        pair<int,int> left = diameter1(root->left);
+        pair<int,int> right = diameter1(root->right);
+        
+        int leftSubTree = left.first; // Left Diameter
+        int rightSubTree = right.first; //Right Diamter
+        int bothSubTree = left.second + right.second ; //Height
+        
+        pair<int,int> ans;
+        ans.first = max(leftSubTree, max(rightSubTree, bothSubTree));
+        ans.second = max(left.second, right.second)+1;
+    
+        return ans; 
     }
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-         if(root == NULL) return 0;
-        
-        int leftSubTree = diameterOfBinaryTree(root->left);
-        int rightSubTree = diameterOfBinaryTree(root->right);
-        int bothSubTree = height(root->left) + height(root->right);
-        
-        int ans = max(leftSubTree, max(rightSubTree,bothSubTree));
-        
-        return ans;
+         return diameter1(root).first;  
 	}
 };
