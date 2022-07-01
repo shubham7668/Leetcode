3b2332854a7d96366c64a46b1acace6aafb1a554
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-07-02 02:10:11 +0530

    Time: 15 ms (78.64%), Space: 22.5 MB (5.07%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -20,18 +20,46 @@ class Solution {
         
         return max(left,right)+1;
     }
+    
+    pair<bool,int> bal(TreeNode* root){
+        if(root==NULL){
+            pair<bool,int> p(true,0);
+            return p;
+        }
+        
+        pair<int,int> left=bal(root->left);
+        pair<int,int> right=bal(root->right);
+        
+        bool leftAns=left.first;
+        bool rightAns=right.first;
+        
+        int diff= abs(left.second-right.second); // <=1
+        
+        pair<bool,int> ans;
+        ans.second=max(left.second,right.second)+1; //height store in 2nd block
+        
+        if(leftAns && rightAns && diff<=1){
+            ans.first=true;
+        }
+        else
+            ans.first=false;
+        
+        return ans;
+    }
 public:
     bool isBalanced(TreeNode* root) {
-        if(root==nullptr) return true;
+//         if(root==nullptr) return true;
         
-        bool left=isBalanced(root->left);
-        bool right=isBalanced(root->right);
+//         bool left=isBalanced(root->left);
+//         bool right=isBalanced(root->right);
         
-        int diff = abs (height(root->left)-height(root->right)) ;
+//         int diff = abs (height(root->left)-height(root->right)) ;
         
-        if(left && right && diff<=1) return true;
-        else
-            return false;
+//         if(left && right && diff<=1) return true;
+//         else
+//             return false;
+        
+        return bal(root).first;
         
     }
 };
