acdba6209257461e6218380c00b00f60f97d1571
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-07-08 01:52:27 +0530

    Time: 13 ms (22.05%), Space: 13.4 MB (59.76%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,23 +11,34 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root, int dist, int level, map<int, vector<pair<int, int>>> &m){
-        if(!root)return;
-        m[dist].push_back(make_pair(level, root->val));
-        dfs(root->left, dist-1, level+1, m);
-        dfs(root->right, dist+1, level+1, m);
-    }
-    
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, vector<pair<int, int>>> m;
-        vector<vector<int>> ans;
         
-        dfs(root, 0, 0, m);
-        for(auto it:m){
-            sort(it.second.begin(), it.second.end());
-            vector<int> tmp;
-            for(auto i:it.second)tmp.push_back(i.second);
-            ans.push_back(tmp);
+        map<int,map<int,multiset<int>>> nodes;
+        
+        queue<pair<TreeNode*,pair<int,int>>> q; //Node, verticalLvl, HorizontalLvl
+        
+        q.push({root,{0,0}});
+        
+        while(!q.empty()){
+            auto curr=q.front();  //node,(vl,hl)
+            q.pop();
+            
+            TreeNode* temp=curr.first;
+            int vl=curr.second.first;  //Vertival Level
+            int hl=curr.second.second; // Horizontal Level
+            
+            nodes[vl][hl].insert(temp->val); //Update Data Structure
+            
+            if(temp->left) q.push({temp->left,{vl-1,hl+1}});
+            if(temp->right) q.push({temp->right,{vl+1,hl+1}});
+        }
+        vector<vector<int>> ans;
+        for(auto i:nodes){
+            vector<int> col;
+            for(auto j:i.second){
+                col.insert(col.end(),j.second.begin(),j.second.end());
+            }
+            ans.push_back(col);
         }
         return ans;
     }
