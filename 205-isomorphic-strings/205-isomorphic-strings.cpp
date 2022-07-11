f874ba3cc0cfd327eac4acb73f2705c1a98c60b2
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-07-12 01:39:58 +0530

    Time: 10 ms (57.00%), Space: 6.9 MB (75.25%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -3,8 +3,9 @@ public:
     bool isIsomorphic(string s, string t) {
         unordered_map<char, int> mp, mp1;
         unordered_map<char, char> mp2;
-        for(int i = 0; i < s.size(); i++){
-            if(mp[s[i]] > 0 or mp1[t[i]] > 0){
+       
+        for(int i = 0; i<s.size();i++){
+            if(mp[s[i]] > 0 || mp1[t[i]] > 0){
                 if(mp2[s[i]] != t[i])
                     return false;
             }
