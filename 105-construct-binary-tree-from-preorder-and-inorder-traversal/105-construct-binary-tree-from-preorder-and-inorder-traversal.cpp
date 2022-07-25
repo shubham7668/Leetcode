090433bd012b7d880a359f79ed8f11d73328381c
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-07-26 02:16:02 +0530

    Time: 82 ms (13.79%), Space: 221.9 MB (5.45%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,9 +10,10 @@
  * };
  */
 class Solution {
+
 public:
     TreeNode* fun(vector<int> pre, vector<int> in ,int &index,
-                  int inStart,int inEnd,int size){
+                  int inStart,int inEnd,int size,map<int,int> &mpp){
         
         //Base case
         if(index>=size || inStart>inEnd) return NULL;
@@ -21,23 +22,29 @@ public:
         TreeNode* root = new TreeNode(element);
         
         //Finding postion in inorder array
-        int pos=-1;
-        for(int i=0;i<size;i++){
-            if(in[i] == element) pos=i;
-        }
+        // int pos=-1;
+        // for(int i=0;i<size;i++){
+        //     if(in[i] == element) pos=i;
+        // }
+        
+        int pos=mpp[element];
         
         //Recursive Calls
-        root->left = fun(pre,in,index,inStart,pos-1,size);
-        root->right = fun(pre,in,index,pos+1,inEnd,size);
+        root->left = fun(pre,in,index,inStart,pos-1,size,mpp);
+        root->right = fun(pre,in,index,pos+1,inEnd,size,mpp);
         
         return root;
     }
     
     TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
+        
         int preIndex=0;
         int n=pre.size();
-        
-        TreeNode* ans=fun(pre,in,preIndex,0,n-1,n);
+        map<int,int> valToInd;
+        for(int i=0;i<n;i++){
+            valToInd[in[i]]=i;
+        }
+        TreeNode* ans=fun(pre,in,preIndex,0,n-1,n,valToInd);
         
         return ans;
     }
