890772f53434135fe8bf7de5971fb6e9a4483165
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-07-27 14:35:11 +0530

    Time: 44 ms (33.93%), Space: 19.2 MB (56.97%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,17 +2,26 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         //brute force
-        vector<int> ans;
-        int cnt=0;
+        // vector<int> ans;
+        // int cnt=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==0) cnt++;
+        //     else
+        //         ans.push_back(nums[i]);
+        // }
+        // while(cnt>0){
+        //     ans.push_back(0);
+        //     cnt--;
+        // }
+        // nums=ans;
+        
+        //Two pointers Approch
+        int j=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==0) cnt++;
-            else
-                ans.push_back(nums[i]);
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
         }
-        while(cnt>0){
-            ans.push_back(0);
-            cnt--;
-        }
-        nums=ans;
     }
 };
