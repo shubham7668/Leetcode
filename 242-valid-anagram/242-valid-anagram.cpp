fb926d3e789b4e866d34ba952b71552643b3e87e
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-07-28 14:08:11 +0530

    Time: 18 ms (40.58%), Space: 7.5 MB (7.79%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int> mpp1;
+        map<char,int> mpp2;
+        for(int i=0;i<s.size();i++){
+            mpp1[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            mpp2[t[i]]++;
+        }
+        if(mpp1==mpp2) return true;
+        else
+            return false;
+    }
+};
