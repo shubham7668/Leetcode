e7c47bc20d701af372e6a7c8c8d6eb9f78fa280a
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-08-03 01:57:47 +0530

    Time: 65 ms (29.63%), Space: 32.7 MB (39.73%) - LeetHub

---
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+//     TreeNode* minimum(TreeNode* root){
+//         TreeNode* temp = root;
+        
+//         if(temp==NULL) return NULL;
+        
+//         while(temp->left){
+//             temp=temp->left;
+//         }
+//         return temp;
+//     }
+    
+    int minimum(TreeNode* root){
+        
+        if(root==NULL) return 0;
+        
+        while(root->left){
+            root=root->left;
+        }
+        return root->val;
+    }
+    
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL) return NULL;
+        
+        if(root->val==key){
+            //0 child
+            if(root->left==NULL && root->right==NULL){
+                delete(root);
+                return NULL;
+            }
+            //1 child
+            
+            //i>Left child
+            if(root->left && root->right==NULL){
+                TreeNode* temp = root->left;
+                delete(root);
+                return temp;
+            }
+            //ii>Right child
+            if(root->left==NULL && root->right){
+                TreeNode* temp = root->right;
+                delete(root);
+                return temp;
+            }
+            
+            //2 child
+            if(root->left && root->right){
+                int mini = minimum(root->right); //Minimum value from right subtree
+                root->val=mini;                  //Copy mini in curr node
+                root->right = deleteNode(root->right,mini);
+                return root;
+            }
+        }
+        
+        if(root->val>key)
+            root->left=deleteNode(root->left,key);
+        else
+            root->right=deleteNode(root->right,key);
+        
+        return root;
+    }
+
+};
