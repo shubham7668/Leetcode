b2a8f6c0384875ff97c0a95f22a6ef081f515d65
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-08-05 01:18:44 +0530

    Time: 410 ms (19.61%), Space: 149.9 MB (18.53%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+
+public:
+void solve(vector<vector<int>> &ans,vector<int> &v,vector<int> temp,int ind,int k){
+    
+    if(v.size()==k){
+        ans.push_back(v);
+        return;
+    }
+    
+    for(int i=ind;i<temp.size();i++){
+        v.push_back(temp[i]);
+        solve(ans,v,temp,i+1,k);
+        v.pop_back();
+    }
+    
+}
+vector<vector<int>> combine(int n, int k) {
+    
+    vector<int> temp(n);
+    for(int i=0;i<n;i++)
+        temp[i]=i+1;
+    vector<vector<int>> ans;
+    vector<int> v;
+    int ind=0;
+    
+    solve(ans,v,temp,ind,k);
+    return ans;
+}   
+};
