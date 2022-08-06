5035fafa07a5ee1b82014f5cd7891a6c5434e773
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-08-07 02:42:02 +0530

    Time: 4 ms (41.31%), Space: 5.9 MB (26.49%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -2,11 +2,17 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         //Brute
-        for(int i=0;i<31;i++){
-            if(pow(2,i)==n) return 1;
-        }
-        return 0;
+        // for(int i=0;i<31;i++){  //2^31 is the max no that an int can store
+        //     if(pow(2,i)==n) return 1;
+        // }
+        // return 0;
         
         //Bit Maniplution
+        if(n<=0) return 0;
+        
+        if((n&(n-1))==0) return 1;
+        else
+            return 0;
+        
     }
 };
