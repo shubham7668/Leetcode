01474240ebc690316507651a8e602f8483f21d63
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-08-16 19:05:41 +0530

    Time: 30 ms (85.00%), Space: 10.5 MB (91.52%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char,int> mp;
-        for(int i=0;i<s.size();i++) mp[s[i]]++;
+//         unordered_map<char,int> mp;
+//         for(int i=0;i<s.size();i++) mp[s[i]]++;
         
-        for(int i=0;i<s.size();i++){
-            if(mp[s[i]]==1) return i;
+//         for(int i=0;i<s.size();i++){
+//             if(mp[s[i]]==1) return i;
+//         }
+//         return -1;
+        
+        //space optimised
+        
+        vector<int> v(26,0);
+        
+        for(int i=0;i<s.length();i++){
+            v[s[i]-'a']++;
+        }
+        for(int i=0;i<s.length();i++){  
+            if(v[s[i]-'a']==1) return i;
         }
         return -1;
     }
