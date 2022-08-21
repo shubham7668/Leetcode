529087ad337664f4f73fe1e3444e83974a8a0dbc
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-08-21 17:12:54 +0530

    Time: 213 ms (14.47%), Space: 69 MB (14.46%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void backtracking(vector<int> nums,int target,
+                      vector<int>& ds,int index,vector<vector<int>> &ans )
+    {
+        if(index==nums.size()){
+            if(target==0) ans.push_back(ds);
+            return;
+        }
+        //Include
+        if(nums[index]<=target){
+            ds.push_back(nums[index]);
+            backtracking(nums,target-nums[index],ds,index,ans);
+            ds.pop_back();
+        }
+        //Exclude
+        backtracking(nums,target,ds,index+1,ans);
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        
+        backtracking(nums,target,ds,0,ans);
+        return ans;
+    }
+};
