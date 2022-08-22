e015616d724e6b430666f965acc75ee7b99beb18
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-08-23 04:13:02 +0530

    Time: 187 ms (62.16%), Space: 93.4 MB (11.45%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-       
-        //Brute Force -> O(n^2)
-        // int maxProfit=0;
-        // for(int i=0;i<prices.size()-1;i++){
-        //     for(int j=i+1;j<prices.size();j++){
-        //         int profit=prices[j]-prices[i];
-        //         maxProfit=max(profit,maxProfit);
+    int maxProfit(vector<int>& nums) {
+        int n=nums.size();
+        //Brute force ->O(n^2)
+        // int maxProfit=INT_MIN;
+        // for(int i=0;i<n-1;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         int profit=nums[j]-nums[i];
+        //         maxProfit=max(maxProfit,profit);
         //     }
         // }
-        //return maxProfit;
+        // if(maxProfit<0) return 0;
+        // return maxProfit;
         
-        //Optimised -> O(n) Basically, I have to minimise buy & maximise profit 
-        int buy=INT_MAX;
-        int profit=0;
-        
-        for(int i=0;i<prices.size();i++){
-            buy=min(buy,prices[i]);
-            profit=max(profit,prices[i]-buy);
+        //Optimised -> O(n)
+        int buy=INT_MAX,profit=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(buy>nums[i]) buy=nums[i];
+            // buy=min(buy,nums[i]);
+            if(profit<nums[i]-buy) profit=nums[i]-buy;
+            // profit=max(profit,nums[i]-buy);
         }
         return profit;
     }
