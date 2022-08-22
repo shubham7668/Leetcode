13db56f42397387624ae95db26381a869b83e9c1
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-08-22 22:51:15 +0530

    Time: 28 ms (32.16%), Space: 13.2 MB (53.23%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        
-        int rows=matrix.size(), cols=matrix[0].size();
-        vector <int> dum1(rows,-1), dum2(cols,-1);
+        //Full optimised
+        int rows=matrix.size(),cols=matrix[0].size();
+        int flag=1;
         
         for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
+            if(matrix[i][0]==0) flag=0;
+            for(int j=1;j<cols;j++){
                 if(matrix[i][j]==0){
-                    dum1[i]=0;
-                    dum2[j]=0;
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
         
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(dum1[i]==0 || dum2[j]==0) matrix[i][j]=0;
+        //Now traverse from backside
+        for(int i=rows-1;i>=0;i--){
+            for(int j=cols-1;j>=1;j--){
+                if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]=0;
             }
-       }
-   }
+            if(flag==0) matrix[i][0]=0;
+        }
+    }
 };
