8ce5dc94952cf3477a6fed3d715f8d04133ad179
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-08-25 02:19:59 +0530

    Time: 47 ms (79.58%), Space: 19.1 MB (66.50%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& v) {
+        sort(v.begin(),v.end());
+        
+        vector<vector<int>> ans;
+        ans.push_back(v[0]);
+        
+        int j=0;
+        for(int i=1;i<v.size();i++){
+            if(v[i][0]<=ans[j][1]) ans[j][1]=max(ans[j][1],v[i][1]);
+            else{
+                j++;
+                ans.push_back(v[i]);
+            }
+        }
+        return ans;
+    }
+};
