dbbdb1e070192a8a6c4acb2bf34e9535c9002e72
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-08-25 03:35:45 +0530

    Time: 165 ms (70.55%), Space: 63.6 MB (23.51%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -8,11 +8,20 @@ public:
         //         return nums[i];
         // return -1;
         
-        //Brute Force2 -> 
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++) mp[nums[i]]++;
+        //Brute Force2 -> tc=O(n)+O(n)   sc=O(n)
+        //unordered_map<int,int> mp;
+        //for(int i=0;i<nums.size();i++) mp[nums[i]]++;
         
-        for(auto i: mp) if(i.second>1) return i.first;
+        //for(auto i: mp) if(i.second>1) return i.first;
+        //return -1;
+        
+        //Optimised 
+        vector<int> hash(nums.size(),0);
+        for(int i=0;i<nums.size();i++){
+            if(hash[nums[i]]!=0) return nums[i];
+            hash[nums[i]]++;
+        }
+        for(int i=0;i<hash.size();i++) if(hash[i]>1) return i+1;
         return -1;
     }
     
