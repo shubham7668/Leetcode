00c065f8d435f0269cd1a97d208844339558583a
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-08-26 18:15:49 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (8.76%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        int maxi=pow(10,9);
+        map<int,vector<int>> mp;
+        
+        for(int i=1;i<=maxi;i=i*2){
+            int temp=i;
+            vector<int> v(10,0);
+            while(temp){
+                int rem=temp%10;
+                v[rem]++;
+                temp=temp/10;
+            }
+            mp[i]=v;
+        }
+        
+        vector<int> now(10,0);
+        while(n){
+            int rem=n%10;
+            now[rem]++;
+            n/=10;
+        }
+        
+        for(int i=1;i<maxi;i=i*2){
+            bool flag=1;
+            vector<int> check=mp[i];
+            for(int j=0;j<10;j++){
+                if(check[j]!=now[j]) flag=0;
+            }
+            if(flag==1) return 1;
+        }
+        return 0;
+    }
+};
