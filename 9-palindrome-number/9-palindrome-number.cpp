4778aca881b34c237fda176a2cec8a94e079fb98
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-08-30 23:17:22 +0530

    Time: 38 ms (13.87%), Space: 5.9 MB (31.53%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -17,11 +17,15 @@ public:
         
         string str=to_string(x);
         int i=0,j=str.size()-1;
-        while(i<=j){
+        if(x<0) return 0;
+        else{
+            while(i<=j){
             if(str[i]!=str[j]) return 0;
             i++;
             j--;
         }
         return 1;
-    }
+        }
+        
+     }
 };
