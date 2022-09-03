f513df67d53435cf292815613ad63d915424901d
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-09-04 00:26:00 +0530

    Time: 7 ms (20.96%), Space: 6.4 MB (26.72%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        set<int> s;
+        for(;;){
+            int temp=0;
+            while(n){
+                int r=n%10;
+                temp+=r*r;
+                n/=10;
+            }
+            n=temp;
+            if(n==1) return 1;
+            auto i=s.find(n);
+            if(i==s.end()) s.insert(n);
+            else return 0;
+         }
+     }
+};
