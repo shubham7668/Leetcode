4922632f771e1e05d4bba0f511962010cb453f69
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-09-08 00:25:45 +0530

    Time: 3 ms (80.30%), Space: 6.7 MB (26.58%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        
+        //Brute
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // for(int i=0;i<t.size();i++){
+        //     if(t[i]!=s[i]) return t[i];
+        // }
+        // return '-1';
+        
+        //Optimised
+        vector<int> v(26,0);
+        
+        for(int i=0;i<s.size();i++) v[s[i]-'a']++;
+        
         for(int i=0;i<t.size();i++){
-            if(t[i]!=s[i]) return t[i];
+            if(v[t[i]-'a']==0) return t[i];
+            
+            else v[t[i]-'a']--;
+            
         }
-        return '-1';
+        return '1';
     }
 };
