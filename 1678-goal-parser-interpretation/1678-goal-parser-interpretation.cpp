6f923f07a8218469d02bd0e5deffd0580d19cfdd
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-09-08 00:18:34 +0530

    Time: 3 ms (45.47%), Space: 6.2 MB (56.63%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string ans="";
+        for(int i=0;i<command.size();i++){ 
+            if(command[i]=='G') ans+='G';
+            else if(command[i]=='(' && command[i+1]==')') ans+='o';
+            else if(command[i]=='a' && command[i+1]=='l') ans+="al";
+        }
+        return ans;
+    }
+};
