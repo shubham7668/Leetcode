357820bb83412a0e515fb09e329e865f6b3f4f36
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-09-09 01:42:17 +0530

    Time: 873 ms (56.86%), Space: 0B (100.00%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select users.name, ifnull(sum(rides.distance),0)as travelled_distance 
+from users 
+left join rides on users.id = rides.user_id
+group by users.id
+order by travelled_distance desc, users.name asc
