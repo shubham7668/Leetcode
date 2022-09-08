2fa837c4f91e99cb258540224c7430ae11fe64f3
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-09-08 17:40:26 +0530

    Time: 3 ms (43.74%), Space: 6.2 MB (54.01%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0;i<s.size();i++){
+            if(int(s[i])>=65 && int(s[i]<=90)){
+                s[i]+=32;
+            }
+        }
+        return s;
+    }
+};
