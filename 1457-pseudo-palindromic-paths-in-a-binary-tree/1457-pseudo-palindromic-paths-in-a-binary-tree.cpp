fd68be98f95116c017d44171b65dd88b45a722cb
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-09-14 23:24:39 +0530

    Time: 674 ms (38.56%), Space: 192.3 MB (28.07%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void rec(TreeNode* root) {
+        if (!root)  return;
+        
+        mp[root->val]++;
+        
+        // If we got to a leaf - check if the path can be a polindrome
+        if (!root->left && !root->right) {
+            int odd = 0;
+            for (auto a : mp)
+                if (a.second % 2 == 1)
+                    odd++;
+
+            if (odd <= 1)
+                res++;
+        }
+        
+        rec(root->left);
+        rec(root->right);
+        mp[root->val]--;
+    }
+    
+    int pseudoPalindromicPaths (TreeNode* root) {
+        rec(root);
+        return res;
+    }
+    
+private:
+    int res = 0;
+    unordered_map<int,int> mp;
+};
