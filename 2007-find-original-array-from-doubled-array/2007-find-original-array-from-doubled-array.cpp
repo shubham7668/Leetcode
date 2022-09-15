02755b2b6691d25b174817235f5e5a5c13e53156
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-09-15 22:19:43 +0530

    Time: 942 ms (18.71%), Space: 144.1 MB (51.60%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,60 +1,38 @@
-// class Solution {
-// public:
-//     vector<int> findOriginalArray(vector<int>& arr) {
-//         //Brute-> O(n^2+nlogn)
-//         int n=arr.size();
-//         if(n%2==1) return {};
-        
-//         sort(arr.begin(),arr.end());
-//         vector<int> ans;
-        
-// //         for(int i=0;i<n-1;i++){
-// //             for(int j=i+1;j<n;j++){
-// //                 if(arr[i]*2==arr[j]){
-// //                     if(arr[j]==-1) continue;
-// //                     ans.push_back(arr[i]);
-// //                     arr[j]=-1;
-// //                     break;
-// //                 }
-// //             }
-// //         }
-// //         if(ans.size()!=n/2) return {};
-// //         return ans;
-        
-//         map<int,int> mp;
-//         for(int i=0;i<n;i++) mp[arr[i]]++;
-//         for(auto i : mp) cout<<i.first<<"->"<<i.second<<endl;
-//         for(int i=0;i<n;i++){
-//             if(mp[arr[i]]==0) continue;
-//             if(mp[arr[i]*2]==0) return {};
-//             ans.push_back(arr[i]);
-//             mp[arr[i]]--;
-//             mp[arr[i]*2]--;
-            
-//         }
-        
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
-    vector<int> findOriginalArray(vector<int>& changed) {
-        int n = changed.size();
-        if (n % 2 == 1) return {};
-        sort(changed.begin(), changed.end());
+    vector<int> findOriginalArray(vector<int>& arr) {
+        //Brute-> O(n^2+nlogn)
+        int n=arr.size();
+        if(n%2==1) return {};
+        
+        sort(arr.begin(),arr.end());
         vector<int> ans;
-        map<int, int> mp;
-        for (int i = 0; i < n; i++) {
-            mp[changed[i]]++;
+        
+//         for(int i=0;i<n-1;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(arr[i]*2==arr[j]){
+//                     if(arr[j]==-1) continue;
+//                     ans.push_back(arr[i]);
+//                     arr[j]=-1;
+//                     break;
+//                 }
+//             }
+//         }
+//         if(ans.size()!=n/2) return {};
+//         return ans;
+        
+        map<int,int> mp;
+        for(int i=0;i<n;i++) mp[arr[i]]++;
+        // for(auto i : mp) cout<<i.first<<"->"<<i.second<<endl;
+        for(int i=0;i<n;i++){
+            if(mp[arr[i]]==0) continue;
+            if(mp[arr[i]*2]==0) return {};
+            ans.push_back(arr[i]);
+            mp[arr[i]]--;
+            mp[arr[i]*2]--;
+            
         }
-        for (int i = 0; i < n; i++) {
-          if (mp[changed[i]] == 0) continue;
-          if (mp[changed[i] * 2] == 0) return {};
-          ans.push_back(changed[i]);
-          mp[changed[i]]--;
-          mp[changed[i] * 2]--;
-        }
+        
         return ans;
     }
 };
