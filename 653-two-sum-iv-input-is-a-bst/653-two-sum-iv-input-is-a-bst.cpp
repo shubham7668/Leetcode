3ac543102ddf009f48e7fbba7bef2002fb9fc604
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-10-09 20:30:05 +0530

    Time: 92 ms (29.61%), Space: 36.9 MB (62.65%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,109 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator{
+    
+    private:
+    
+    stack<TreeNode*>S;
+    bool reverse;
+    
+    public:
+    
+    BSTIterator(TreeNode*root,bool isReverse){
+        
+        reverse = isReverse;
+        
+        pushAll(root);
+        
+    }
+    
+    void pushAll(TreeNode*root){
+        
+        if(!root) return;
+        
+        while(root){
+            
+            S.push(root);
+            
+            if(reverse){
+                root=root->right;
+            }else{
+                root=root->left;
+            }
+            
+        }
+        
+    }
+    
+    int next(){
+        
+        TreeNode * curr = S.top();
+        S.pop();
+        
+        int value = curr->val;
+        
+        if(reverse){
+            
+            pushAll(curr->left);
+            
+        }
+        
+        else{
+            
+            pushAll(curr->right);
+            
+        }
+        
+        return value;
+        
+    }
+    
+    bool hasNext(){
+        
+        return !S.empty();
+        
+    }
+    
+};
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        
+        if(!root) return false;
+        
+        BSTIterator l(root,false);
+        BSTIterator r(root,true);
+        
+        int i = l.next();
+        int j = r.next();
+        
+        while(i<j){
+            
+            if(i+j==k){
+                return true;
+            }
+            
+            else if(i+j>k){
+                j = r.next();
+            }
+            
+            else{
+                i = l.next();
+            }
+            
+        }
+        
+        return false;
+        
+    }
+};
