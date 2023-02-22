5cecd5c9c99a42ca871d54a307a0a22563a6abc6
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2023-02-22 18:12:54 +0530

    Time: 46 ms (90.60%), Space: 26 MB (77.07%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,28 +1,54 @@
+// class Solution {
+// public:
+//     int shipWithinDays(vector<int>& weights, int days) {
+//         int maxWeight = -1, totalWeight = 0;
+//         for (int weight : weights) {
+//             maxWeight = max(maxWeight, weight);
+//             totalWeight = totalWeight + weight;
+//         }
+//         //here weight and total weight work as left and right pointer of bunary search
+//         while (maxWeight < totalWeight) {
+//             int midWeight = maxWeight + (totalWeight - maxWeight) / 2;
+//             int daysNeeded = 1, currWeight = 0;
+//             for (int weight : weights) {
+//                 if (currWeight + weight > midWeight) {
+//                     daysNeeded++;
+//                     currWeight = 0;
+//                 }
+//                 currWeight = currWeight + weight;
+//             }
+//             if (daysNeeded > days) {
+//                 maxWeight = midWeight + 1;
+//             } else {
+//                 totalWeight = midWeight;
+//             }
+//         }
+//         return maxWeight;
+//     }
+// };
+
 class Solution {
 public:
-    int shipWithinDays(vector<int>& weights, int days) {
-        int maxWeight = -1, totalWeight = 0;
-        for (int weight : weights) {
-            maxWeight = max(maxWeight, weight);
-            totalWeight = totalWeight + weight;
-        }
-        //here weight and total weight work as left and right pointer of bunary search
-        while (maxWeight < totalWeight) {
-            int midWeight = maxWeight + (totalWeight - maxWeight) / 2;
-            int daysNeeded = 1, currWeight = 0;
-            for (int weight : weights) {
-                if (currWeight + weight > midWeight) {
-                    daysNeeded++;
-                    currWeight = 0;
+    int shipWithinDays(std::vector<int>& weights, int days) {
+        int left = *std::max_element(weights.begin(), weights.end()); // minimum capacity needed
+        int right = std::accumulate(weights.begin(), weights.end(), 0); // maximum capacity needed
+        while (left < right) {
+            int mid = (left + right) / 2;
+            int current = 0;
+            int required_days = 1;
+            for (int w : weights) {
+                if (current + w > mid) {
+                    current = 0;
+                    ++required_days;
                 }
-                currWeight = currWeight + weight;
+                current += w;
             }
-            if (daysNeeded > days) {
-                maxWeight = midWeight + 1;
+            if (required_days > days) {
+                left = mid + 1;
             } else {
-                totalWeight = midWeight;
+                right = mid;
             }
         }
-        return maxWeight;
+        return left;
     }
 };
