00ab56e81392828d6fe4052fbe8b5ab14d16e474
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2023-04-01 11:13:37 +0530

    Time: 45 ms (27.23%), Space: 42.8 MB (18.34%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int binary(vector<int> nums,int s,int e,int target){
+      
+        if(s<=e){
+            int mid=s+(e-s)/2;
+            if(target==nums[mid]) return mid;
+            
+            else if(target>nums[mid]) 
+                return binary(nums,mid+1,e,target);
+            
+            else
+                return binary(nums,s,mid-1,target);
+        }
+        
+        return -1; //target not found
+    }
+    
+    int search(vector<int>& nums, int target) {
+        //Iterative
+        int start=0,end=nums.size()-1;
+        
+//         while(start<=end){
+//             int mid=start+(end-start)/2;
+            
+//             if(target==nums[mid]) return mid;
+            
+//             if(target>nums[mid]) start=mid+1;
+//             else
+//                 end=mid-1;
+//         }
+//         return -1;
+        
+        //Recursive
+        return binary(nums,start,end,target);
+    }
+};
