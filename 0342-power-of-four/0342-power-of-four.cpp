425da75576c16b646a7c7942aab71ba3e30fe12f
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2023-10-23 09:46:46 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (13.31%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        vector<int> v = {1,4,16,64,256,1024,4096,16384,65536,262144,1048576,
+                          4194304,16777216,67108864,268435456,1073741824};
+        for(int i:v) if(n==i)return true;
+        return false;
+        
+    }
+};
